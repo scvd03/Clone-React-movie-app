2363f65befca55b86e5676cb26e2833c19bb1fe1
scvd03 <scvd03@gmail.com>
2020-11-15 21:18:45 +0900

    fetching from yts proxy api & rendering

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
 import React from 'react';
+import axios from 'axios';
+import Movie from './Movie';
 
 class App extends React.Component {
 
   state = {
-    isLoading: true
+    isLoading: true,
+    movies: []
+  };
+
+  getMovie = async () => {
+    const {data: { data: {movies}}} = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    this.setState({movies: movies, isLoading: false});
   };
 
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({isLoading: false});
-    }, 6000);
+    this.getMovie();
   }
 
   render() {
-    const { isLoading } = this.state;
-    return <div>{isLoading ? "Loading...." : "We are ready"}</div>;
+    const { isLoading, movies } = this.state;
+    return <div>{isLoading ? "Loading...." 
+    : movies.map(movie => (
+      <Movie
+        key={movie.id}
+        id={movie.id}
+        year={movie.year}
+        title={movie.title}
+        summary={movie.summary}
+        poster={movie.medium_cover_image}
+    />
+    ))}</div>;
   }
 }
 
