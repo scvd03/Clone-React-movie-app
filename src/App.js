b7794cf3af57b587be5bad702ac39d195446f013
scvd03 <scvd03@gmail.com>
2020-12-09 19:37:55 +0900

    props between Routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { HashRouter, Route } from "react-router-dom";
 import About from "./routes/About";
 import Home from './routes/Home';
+import Detail from './routes/Detail';
 import Navigation from './components/Navigation';
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navigation />
       <Route path="/about" component={About} />
       <Route path="/" exact={true} component={Home} />
+      <Route path="/movie-detail" exact={true} component={Detail} />
     </HashRouter>
   )
 }
